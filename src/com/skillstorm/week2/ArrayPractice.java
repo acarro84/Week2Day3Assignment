84c23adb72eaa33de8edae387f337dd606fd5283
acarro84 <123746711+acarro84@users.noreply.github.com>
2023-04-06 15:39:00 -0500

    This is the Array assignment for week 2 day 4 with findMax and findMin

--- src/com/skillstorm/week2/ArrayPractice.java
+++ src/com/skillstorm/week2/ArrayPractice.java
@@ -3,19 +3,48 @@ package com.skillstorm.week2;
 public class ArrayPractice {
 
 	public static void main(String[] args) {
-		String[] myFavoriteFoods= {"pizza" , "pasta" , "jamabalaya" , "steak" , "barbecue"};
-		
-		System.out.println("My number 1 favorite food is: " + myFavoriteFoods[0] );
-		System.out.println("My number 2 favorite food is: " + myFavoriteFoods[1] );
-		System.out.println("My number 3 favorite food is: " + myFavoriteFoods[2] );
-		System.out.println("My number 4 favorite food is: " + myFavoriteFoods[3] );
-		System.out.println("My number 5 favorite food is: " + myFavoriteFoods[4] );
-		myFavoriteFoods = new String [] {"salad" , "pasta" , "jamabalaya" , "steak" , "barbecue"};
-		System.out.println("My new favorite food is " + myFavoriteFoods[0] + " because my doctor said to stop eating pizza.");
-		
+		int[] numbers = { -1, -17, -17, -4, -5 };
+
+		System.out.println("The maximum value in numbers[] is: " + findMaxImproved(numbers));
+		System.out.println("The maximum value in numbers[] is: " + findMaxImproved(null));
+		System.out.println("The minimum value in numbers[] is: " + findMin(numbers));
+		System.out.println("The minimum value in numbers[] is: " + findMin(null));
 		
 		
 
 	}
+
+	public static int findMaxImproved(int[] numbers) {
+
+		int index = 0;
+		int max = Integer.MIN_VALUE;
+		if (numbers == null || numbers.length <= 0) // preferred way to do null check and length check
+			return Integer.MIN_VALUE;
+		else {
+		while (index < numbers.length) {
+			max = Math.max(max, numbers[index]);
+			index++;
+		}
+		
+		return max;
+		}
+	}
+	public static int findMin(int[] numbers) {
+		int index = 0;
+		int min = Integer.MAX_VALUE;
+		if (numbers == null || numbers.length <= 0) 
+			return Integer.MAX_VALUE;
+		else {
+			while (index < numbers.length) {
+				min = Math.min(min,  numbers[index]);
+				index++;
+			}
+			
+		
+		return min;
+		}
+		
+	}
 	
+
 }
